3669ca6b6c7ce654df0af60e7c0f2f02c6e45dd7
Joshua Töpfer <joshua.toepfer@innoq.com>
2021-10-20 10:31:42 +0200

    completed assigment 2

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 console.log("Hooray, my first line of JavaScript!");
+
+const TYPE_USER = "user";
+const TYPE_SYSTEM = "system";
+
+const m1 = {textBody: "Alice joined the chat",
+            sender: "", 
+            type: TYPE_SYSTEM};
+
+const m2 = {textBody: "Bob joined the chat",
+            sender: "", 
+            type: TYPE_SYSTEM};
+
+const m3 = {textBody: "Hello Alice, how are you?",
+            sender: "Bob", 
+            type: TYPE_USER};
+
+const m4 = {textBody: "Hi Bob, I'm fine. How are you?",
+            sender: "Alice", 
+            type: TYPE_USER};
+
+sendMessage(m1);
+sendMessage(m2);
+sendMessage(m3);
+sendMessage(m4);
+
+function sendMessage({textBody, sender, type}){
+    if(type === TYPE_USER){
+        console.log(`${sender}: ${textBody}`)
+    } else if(type == TYPE_SYSTEM) {
+        console.log(`...${textBody}...`)
+    }
+}
