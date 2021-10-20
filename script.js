6033e778166f17cc946fb3098c17659278559bb1
Joshua Töpfer <joshua.toepfer@innoq.com>
2021-10-20 15:49:31 +0200

    completed assignment 6

--- script.js
+++ script.js
@@ -1,62 +1,77 @@
-function Message(textBody) {
-    this.textBody = textBody;
+class Message {
+    constructor(textBody) {
+        this.textBody = textBody;
+    }
+
+    render() {
+        return `${this.sender}: ${this.textBody}`; 
+    }
 }
 
-Message.prototype.render = function(){console.log(this.textBody);}
+class UserMessage extends Message {
+    constructor(sender, textBody) {
+        super(textBody);
+        this.sender = sender;
+    }
 
-function UserMessage(sender, textBody) {
-    Message.call(this, textBody);
-    this.sender = sender;
+    render() {
+        return `${this.sender}: ${this.textBody}`;
+    }
 }
 
-UserMessage.prototype = Object.create(Message.prototype);
-UserMessage.prototype.constructor = UserMessage;
-UserMessage.prototype.render = function(){console.log(`${this.sender}: ${this.textBody}`)}
+class SystemMessage extends Message {
+    constructor(textBody) {
+        super(textBody);
+    }
 
-function SystemMessage(textBody) {
-    Message.call(this, textBody);
+    render() {
+        return `...${this.textBody}...`;
+    }
 }
 
-SystemMessage.prototype = Object.create(Message.prototype);
-SystemMessage.prototype.constructor = SystemMessage;
-SystemMessage.prototype.render = function(){console.log(`...${this.textBody}...`)}
+class Chat {
+    constructor() {
+        this.messages = [];
+    }
 
-const messages = [new SystemMessage("Alice joined the chat"),
+    sendMessage(message) {
+        this.messages.push(message);
+        console.log(message);
+        if(message.render){
+            console.log(message.render());
+        }
+    }
+
+    members() {
+        return this.messages.filter(message => message instanceof UserMessage).map(message => message.sender).reduce((previousValue, currentValue) => {
+            if(!previousValue.includes(currentValue)){
+                previousValue.push(currentValue);
+            }
+            return previousValue;
+        },[]);
+    }
+
+    wordsPerMember() {
+        return this.messages.filter(message => message instanceof UserMessage).reduce((previousValue, currentValue) => {
+            if(previousValue[currentValue.sender]){
+                previousValue[currentValue.sender] += currentValue.textBody.split(" ").length;
+            } else {
+                previousValue[currentValue.sender] = currentValue.textBody.split(" ").length;
+            }
+            return previousValue;
+        }, {});
+    }
+}
+
+const chat = new Chat();
+const initialMessages = [new SystemMessage("Alice joined the chat"),
                   new SystemMessage("Bob joined the chat"),
                   new UserMessage("Bob", "Hello Alice, how are you?"),
                   new UserMessage("Alice", "Hi Bob, I'm fine. How are you?"),
                   new UserMessage("Bob", "I'm fine too."),
                 ];
 
-for (const message of messages) {
-    sendMessage(message);            
-}
+initialMessages.forEach(message => chat.sendMessage(message));
 
-messages.forEach(message => sendMessage(message));
-
-const members = messages.filter(message => message instanceof UserMessage).map(message => message.sender).reduce((previousValue, currentValue) => {
-    if(!previousValue.includes(currentValue)){
-        previousValue.push(currentValue);
-    }
-    return previousValue;
-},[]);
-
-console.log(members);
-
-const numberOfWordsPerMember = messages.filter(message => message instanceof UserMessage).reduce((previousValue, currentValue) => {
-    if(previousValue[currentValue.sender]){
-        previousValue[currentValue.sender] += currentValue.textBody.split(" ").length;
-    } else {
-        previousValue[currentValue.sender] = currentValue.textBody.split(" ").length;
-    }
-    return previousValue;
-}, {});
-console.log(numberOfWordsPerMember);
-
-
-function sendMessage(message){
-    console.log(message);
-    if(message.render){
-        console.log(message.render());
-    }
-}
+console.log(chat.members());
+console.log(chat.wordsPerMember());
