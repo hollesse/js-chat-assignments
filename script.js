3d4afef96d9730cb765b393c6c0a17391ccaefc5
Joshua Töpfer <joshua.toepfer@innoq.com>
2021-10-20 10:47:21 +0200

    completed assignment 3

--- script.js
+++ script.js
@@ -3,31 +3,29 @@ console.log("Hooray, my first line of JavaScript!");
 const TYPE_USER = "user";
 const TYPE_SYSTEM = "system";
 
-const m1 = {textBody: "Alice joined the chat",
-            sender: "", 
-            type: TYPE_SYSTEM};
+const createMessage = (sender, text, type) => ({textBody: text, sender, type, render});
 
-const m2 = {textBody: "Bob joined the chat",
-            sender: "", 
-            type: TYPE_SYSTEM};
-
-const m3 = {textBody: "Hello Alice, how are you?",
-            sender: "Bob", 
-            type: TYPE_USER};
-
-const m4 = {textBody: "Hi Bob, I'm fine. How are you?",
-            sender: "Alice", 
-            type: TYPE_USER};
+const m1 = createMessage("", "Alice joined the chat", TYPE_SYSTEM);
+const m2 = createMessage("", "Bob joined the chat", TYPE_SYSTEM);
+const m3 = createMessage("Bob", "Hello Alice, how are you?", TYPE_USER);
+const m4 = createMessage("Alice", "Hi Bob, I'm fine. How are you?", TYPE_USER);
 
 sendMessage(m1);
 sendMessage(m2);
 sendMessage(m3);
 sendMessage(m4);
 
-function sendMessage({textBody, sender, type}){
-    if(type === TYPE_USER){
-        console.log(`${sender}: ${textBody}`)
-    } else if(type == TYPE_SYSTEM) {
-        console.log(`...${textBody}...`)
+function sendMessage(message){
+    console.log(message);
+    if(message.render){
+        console.log(message.render());
+    }
+}
+
+function render(){
+    if(this.type === TYPE_USER){
+        return `${this.sender}: ${this.textBody}`;
+    } else if(this.type == TYPE_SYSTEM) {
+        return `...${this.textBody}...`;
     }
 }
